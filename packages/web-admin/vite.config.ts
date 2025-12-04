d5a6e65a1ef8ad19789a42977fa40530b895a899
zzz <zzz@qq.com>
2025-12-04 18:23:39 +0800

    提交

--- packages/web-admin/vite.config.ts
+++ packages/web-admin/vite.config.ts
@@ -22,8 +22,8 @@ export default defineConfig({
     },
     css: {
         preprocessorOptions: {
-            sass: {
-                additionalData: `@import "@/styles/variables.scss"`,
+            scss: {
+                additionalData: `@import "@/styles/variables.scss";`,
             },
         },
     },
